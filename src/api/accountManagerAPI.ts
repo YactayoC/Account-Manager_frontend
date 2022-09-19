599f5d3974b97e6c87d128cb2dc62dbbb4ba6e72
Sebastian Yactayo <syactayocrisostomo@gmail.com>
2022-09-18 19:02:32 -0500

    add custom hooks for register and login user

--- src/api/accountManagerAPI.ts
+++ src/api/accountManagerAPI.ts
@@ -1,7 +1,4 @@
 import axios from 'axios';
-export const { REACT_APP_API_URL } = process.env
-const API_URL = REACT_APP_API_URL ?? ""
-console.log(API_URL)
 
 const accountManagerAPI = axios.create({
   baseURL: 'https://account-managerbackend-production.up.railway.app/api',
